741388f612eb3920bbdac2b75a6036438e82c24f
victor Muriithi <victornjiru80@gmail.com>
2025-07-08 16:23:02 +0300

    added a frontend link

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ connectDB()
 const allowedOrigins = [
   'http://localhost:5173',
   'https://instant-aqi-client.vercel.app',
+  'https://instant-aqi-client-git-main-victor-muriithi-d35b20d0.vercel.app',
 ];
 
 app.use(cors({
